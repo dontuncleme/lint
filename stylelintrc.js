c0feec284d26892907e5dc8d2865ffc4a54ba08c
Sergey Samsonov <ssamsonov@yandex-team.ru>
2021-08-22 00:17:22 +0300

    stylelint update

--- stylelintrc.js
+++ stylelintrc.js
@@ -230,6 +230,7 @@ module.exports = {
                     'grid-row',
                     'grid-row-end',
                     'grid-row-start',
+                    'gap',
                 ],
             },
             {
