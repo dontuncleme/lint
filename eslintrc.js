13ec9fd82f7b3455f36ad965831efd4297d4d802
Sergey Samsonov <ssamsonov@yandex-team.ru>
2021-09-15 21:50:59 +0300

    add simple-import-sort

--- eslintrc.js
+++ eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
         es6: true,
         node: true,
     },
-    plugins: ['ascii', '@typescript-eslint', 'import'],
+    plugins: ['ascii', '@typescript-eslint', 'import', 'simple-import-sort'],
     rules: {
         indent: [
             2,
@@ -141,6 +141,23 @@ module.exports = {
         'import/no-useless-path-segments': [2],
         'import/no-anonymous-default-export': ['error', { allowCallExpression: false }],
         'import/first': [1],
+
+        'simple-import-sort/imports': [
+            'error',
+            {
+                groups: [
+                    // Node.js builtins. You could also generate this regex if you use a `.js` config
+                    // For example: `^(${require("module").builtinModules.join("|")})(/|$)`
+                    [
+                        '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+                    ],
+                    // Parent imports. Put `..` last
+                    ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                    // Other relative imports. Put same-folder imports and `.` last
+                    ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                ],
+            },
+        ],
     },
     overrides: [
         {
